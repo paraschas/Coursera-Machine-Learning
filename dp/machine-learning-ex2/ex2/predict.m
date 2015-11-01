d470d550938800a1737bd44fcd89a78386381be7
Dimitrios Paraschas <paraschas@gmail.com>
2015-11-01 12:07:09 +0200

    implemented predict function

--- dp/machine-learning-ex2/ex2/predict.m
+++ dp/machine-learning-ex2/ex2/predict.m
@@ -4,7 +4,8 @@ function p = predict(theta, X)
 %   p = PREDICT(theta, X) computes the predictions for X using a
 %   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
 
-m = size(X, 1); % Number of training examples
+% number of training examples
+m = size(X, 1);
 
 % You need to return the following variables correctly
 p = zeros(m, 1);
@@ -15,13 +16,19 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+h_theta = theta' * X';
 
-
-
-
-
+for i = 1:m
+    %h_theta(i)
+    %sigmoid(h_theta(i))
+    if (sigmoid(h_theta(i)) >= 0.5)
+        p(i) = 1;
+    else
+        p(i) = 0;
+    end
+    %p(i)
+end
 
 % =========================================================================
-
 
 end

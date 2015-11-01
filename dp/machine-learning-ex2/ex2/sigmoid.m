f0ee566fbc0e21d7f0dea46e63cb3a39955a105b
Dimitrios Paraschas <paraschas@gmail.com>
2015-11-01 09:23:43 +0200

    whitespace

--- dp/machine-learning-ex2/ex2/sigmoid.m
+++ dp/machine-learning-ex2/ex2/sigmoid.m
@@ -2,7 +2,7 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================

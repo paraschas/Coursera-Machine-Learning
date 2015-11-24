3cf005a9a83edd5220de7dd124f8ecdcd5c6b9df
Dimitrios Paraschas <paraschas@gmail.com>
2015-11-24 17:16:53 +0200

    implemented Learning Curve

--- dp/machine-learning-ex5/ex5/learningCurve.m
+++ dp/machine-learning-ex5/ex5/learningCurve.m
@@ -51,15 +51,17 @@ error_val   = zeros(m, 1);
 %       end
 %
 
-% ---------------------- Sample Solution ----------------------
+for i = 1:m
+    theta = trainLinearReg(X(1:i, :), y(1:i), lambda);
+    % size(theta) == [2 1]
 
+    % linearRegCostFunction prototype
+    % function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 
+    error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
 
-
-
-
-
-% -------------------------------------------------------------
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 % =========================================================================
 

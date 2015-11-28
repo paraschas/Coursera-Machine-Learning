eabc5520caf1e6225366b0b5e61409573f833a64
Dimitrios Paraschas <paraschas@gmail.com>
2015-11-28 14:44:10 +0200

    implemented Parameters (C, sigma) for Dataset 3

--- dp/machine-learning-ex6/ex6/dataset3Params.m
+++ dp/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,47 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% DEBUG
+%test_values = [0.1 1];
+test_values = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+% measure the time taken by the for loop
+% start
+% http://www.mathworks.com/help/matlab/ref/tic.html
+%tic
 
+errors = [];
+for i = 1:length(test_values)
+    C = test_values(i);
+    for j = 1:length(test_values)
+        sigma = test_values(j);
+        % DEBUG
+        %fprintf('training the SVM with C = %f and sigma = %f\n', C, sigma);
+        % function [model] = svmTrain(X, Y, C, kernelFunction, tol, max_passes)
+        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        % function pred = svmPredict(model, X)
+        predictions = svmPredict(model, Xval);
+        % http://stackoverflow.com/questions/781410/appending-a-vector-to-an-empty-matlab-matrix
+        errors(end+1, :) = [C sigma mean(double(predictions ~= yval))];
+    end
+end
 
+% measure the time taken by the for loop
+% end
+% http://www.mathworks.com/help/matlab/ref/toc.html
+%toc
 
+[min_error index] = min(errors(:, 3));
+% min_error == 0.03
+% index == 35
+C = errors(index, 1);
+% C == 1.0
+sigma = errors(index, 2);
+% sigma == 0.1
 
+% DEBUG
+%errors
+%fprintf('minimum error is %f at index %f with C = %f and sigma = %f\n', min_error, index, C, sigma);
 
 % =========================================================================
 

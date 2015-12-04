98b2c53f9787a41456aeba3fb2de2764ff47b691
Dimitris Akridas <d.akridas@gmail.com>
2015-12-04 21:38:41 +0200

    Ex8:  Collaborative filtering gradient

--- da/machine-learning-ex8/ex8/cofiCostFunc.m
+++ da/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -42,16 +42,8 @@ Theta_grad = zeros(size(Theta));
 
 J = (1/2) * sum(([X*Theta'] - Y)(R == 1).^2);
 
-
-
-
-
-
-
-
-
-
-
+X_grad = (([X*Theta'] - Y).*R)*Theta;
+Theta_grad = (([X*Theta'] - Y).*R)' * X;
 
 
 

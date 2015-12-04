a1ffee9e76f1306952382dbb68ae6f0b31272a00
Dimitris Akridas <d.akridas@gmail.com>
2015-12-04 21:46:12 +0200

    Ex8: Regularized cost function

--- da/machine-learning-ex8/ex8/cofiCostFunc.m
+++ da/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,7 +40,7 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-J = (1/2) * sum(([X*Theta'] - Y)(R == 1).^2);
+J = (1/2) * sum(([X*Theta'] - Y)(R == 1).^2) + (lambda/2).*(sum(sum(Theta.^2)) + sum(sum(X.^2)));
 
 X_grad = (([X*Theta'] - Y).*R)*Theta;
 Theta_grad = (([X*Theta'] - Y).*R)' * X;
